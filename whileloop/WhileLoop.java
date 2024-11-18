1f04eeeaeb5dc6d19cddfdf6200ed23788fc32cf
jyoti <harbade.jyoti@gmail.com>
2024-11-18 18:01:31 +0530

    loop18thnov

---
+++ whileloop/WhileLoop.java
@@ -0,0 +1,17 @@
+package whileloop;
+
+public class WhileLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=10;
+while(a>6)
+{
+	int b= 20;
+	int c=b+20;
+	System.out.println(c);
+	a--;
+}
+	}
+
+}
